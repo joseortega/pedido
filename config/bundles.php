56db41e1d91ac6d6d410be2e83400903c972e460
Jose Ortega <josheorteg@gmail.com>
2018-10-22 15:28:39 -0500

    instalado bundle para deploy

--- config/bundles.php
+++ config/bundles.php
@@ -21,4 +21,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyDeployBundle\EasyDeployBundle::class => ['dev' => true, 'test' => true],
 ];
